0f9cbe2cdf3a01aa20666e1b9b80870fb550d121
WHITES12 <118122618+WHITES12@users.noreply.github.com>
2023-06-16 23:59:38 +0530

    Update index.jsx
    Signed-off-by: WHITES12 <118122618+WHITES12@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -29,7 +29,7 @@ export default function Index() {
   <div className="py-10"></div>
   <Link href={"https://youtube.com/@wantedchandranvaat9636"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-<i className="fab fa-youtube-play mr-2" />Youtube
+<i className="fab fa-youtube mr-2" />Youtube
     </a>
   </Link>
         </div>
