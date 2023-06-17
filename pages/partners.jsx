4b1af8b8edfe78671eabafe31b9c3022dd79b7a3
WHITES12 <118122618+WHITES12@users.noreply.github.com>
2023-06-17 12:24:20 +0530

    Update partners.jsx
    Signed-off-by: WHITES12 <118122618+WHITES12@users.noreply.github.com>

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,14 +16,14 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://discord.gg/JaNCZvPxMQ">
+            <a href="https://discord.gg/MYxwrfNyx2">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="" />
+                            <img className="rounded-full h-32 w-32" src="/img/mod.png" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">None</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                        None!
+                                        
                                     </p>
                                 </div>     
                             </div>
@@ -31,21 +31,6 @@ export default function Partners() {
                     </a>
                     </div>
 
-                    <div className="flex flex-col justify-center text-white rounded">
-                        <a href="https://discord.gg/JaNCZvPxMQ">
-                            <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="" />
-                            <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">None</h1>        
-                                <div className="flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                    None!
-                                    </p>
-                                </div>     
-                            </div>
-                        </div>
-                        </a>
-                        </div>
 
                 </div>
                 <div className="py-10"></div>
