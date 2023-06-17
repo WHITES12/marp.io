1ed0f9742a9ad5dfcd952aa1ca386dfb95621954
WHITES12 <118122618+WHITES12@users.noreply.github.com>
2023-06-17 12:59:34 +0530

    Update developers.jsx
    Signed-off-by: WHITES12 <118122618+WHITES12@users.noreply.github.com>

--- pages/developers.jsx
+++ pages/developers.jsx
@@ -12,19 +12,6 @@ export default function Developers() {
       <div className="flex justify-center">
         <DiscordWidget imageUrl="https://discord.c99.nl/widget/theme-3/961941181879709696.png" />
       </div>
-    </div>
-  );
-}
-
-<div className="container mx-auto mt-10">
-      <div className="flex justify-center">
-        <img className="w-64" src="/img/bck4.png" alt="Background" />
-      </div>
-      <h1 className="py-7 text-4xl font-extrabold text-center text-white">
-        <i className="fal fa-stars text-amber-400 mr-2" />
-          Developers
-        </h1>
-
       <div className="flex justify-center">
         <DiscordWidget imageUrl="https://discord.c99.nl/widget/theme-3/985079528344915989.png" />
       </div>
