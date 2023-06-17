476e2f20192d9f166856b65b80baffe1a1c93ece
WHITES12 <118122618+WHITES12@users.noreply.github.com>
2023-06-17 13:00:06 +0530

    Update partners.jsx
    Signed-off-by: WHITES12 <118122618+WHITES12@users.noreply.github.com>

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -18,7 +18,7 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://discord.gg/MYxwrfNyx2">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-35 w-35" src="/img/mod.png" />
+                            <img className="rounded-full h-32 w-32" src="/img/mod.png" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Masm</h1>        
                                 <div className="flex items-center mt-1">
